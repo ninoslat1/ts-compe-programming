0291c35d65bea6b68fb1d6fd510a79218bc90d11
ninoslat1 <benedict.ellnina@gmail.com>
2023-10-16 14:55:48 +0700

    first commit

---
+++ src/fizzbuzz.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer"
+import { FizzBuzz } from "../type.ts"
+
+let i:number = 1
+let f:number = 3
+let b:number = 5
+
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "length",
+        message: "Pick a number"
+    }
+])
+
+const Fizzbuzz:FizzBuzz = (length:number) => {
+    for(i; i <= length; i++){
+        if(i%b === 0 && i%f === 0){
+            console.log("FizzBuzz")
+        }
+        else if(i%f === 0){
+            console.log("Fizz")
+        }
+        else if(i%b === 0){
+            console.log("Buzz")
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
+
+const {length} = answer
+
+length !== 0 ? Fizzbuzz(length) : console.log("Please insert a number")
+
